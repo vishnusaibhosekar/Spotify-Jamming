f471d4993e7e37f525032a8818e31daa1a57b2ff
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2023-04-06 02:03:42 -0400

    adds logic to rename playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,10 +3,21 @@ import "./Playlist.css";
 import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { playlistName: 'New Playlist' };
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event) {
+        this.setState({ playlistName: event.target.value });
+        this.props.onNameChange(event.target.value);
+    }
+
     render() {
         return (
             <div className="Playlist">
-                <input value="New Playlist"/>
+                <input value={this.state.playlistName} onChange={this.handleNameChange} />
                 <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
